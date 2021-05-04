a1b07035b1bd6cf3de82783677fcdb7d8cfbf530
Matthaios Stioukis <stioukimattigr@gmail.com>
2021-05-04 22:30:20 +0300

    I have to make the routes of my programm

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # see all restaurants
+  # create a restaurant and redirect to the show view of that restaurant
+  # see the all reviews asociated to a restaurant
+  # add a review to a restaurant
+
 end
